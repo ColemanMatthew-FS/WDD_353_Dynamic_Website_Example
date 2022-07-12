8c0deccbc9a5b640c07e5bdf677e13ccb3785409
ColemanMatthew-FS <MRColeman1@student.fullsail.edu>
2022-07-12 15:10:58 -0400

    Slightly adjusted text

--- routes/routes.js
+++ routes/routes.js
@@ -23,7 +23,7 @@ router.get("/register",(req,res)=>{
 router.post("/register",(req,res)=>{
     errors = validation(req.body)
     res.render('register', {
-        pagename: 'Registration Reload',
+        pagename: 'Registration',
         errors: errors
       });
 });
